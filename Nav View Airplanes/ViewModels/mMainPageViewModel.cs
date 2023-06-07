3f36b093ef7ac81db6ed857147902eb2b5cfd0bd
user_12_19P1 <muhametova.alcu@example.com>
2023-06-07 11:59:03 +0500

    07.06

--- Nav View Airplanes/ViewModels/mMainPageViewModel.cs
+++ Nav View Airplanes/ViewModels/mMainPageViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using DevExpress.Mvvm;
+using GMap.NET.WindowsPresentation;
 using Nav_View_Airplanes.Services;
 using Nav_View_Airplanes.Views;
 using System;
@@ -6,7 +7,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
 
 namespace Nav_View_Airplanes.ViewModels
 {
@@ -14,11 +18,27 @@ namespace Nav_View_Airplanes.ViewModels
     {
         private readonly PageService _pageService;
 
+        public GMapControl gMapControl { get; set; }
+
         public mMainPageViewModel(PageService pageService)
         {
             _pageService = pageService;
+            gMapControl = new GMapControl();
+            GMap.NET.GMaps.Instance.Mode = GMap.NET.AccessMode.ServerAndCache;
+            gMapControl.MapProvider = GMap.NET.MapProviders.OpenStreetMapProvider.Instance;
+            gMapControl.MinZoom = 2;
+            gMapControl.MaxZoom = 17;
+            gMapControl.Zoom = 4;
+            gMapControl.ShowCenter = false;
+            gMapControl.MouseWheelZoomType = GMap.NET.MouseWheelZoomType.MousePositionWithoutCenter;
+            gMapControl.CanDragMap = true;
+            gMapControl.ClipToBounds= true;
+            gMapControl.DragButton = MouseButton.Left;
+            gMapControl.SetPositionByKeywords("Russia");
+
+            //gMapControl.EmptyMapBackground = new SolidColorBrush(Color.FromRgb(170, 211, 223));
+            gMapControl.EmptyTileBrush = new SolidColorBrush(Colors.Gainsboro);
+            gMapControl.EmptyTileBorders = new Pen(new SolidColorBrush(Colors.Gray), 2);
         }
-
-
     }
 }
